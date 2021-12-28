f2af384ac2febc33b5dec949cd45a8ed4f42a9b2
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-12-27 23:19:10 -0800

    added status flags

--- auth_server_main.js
+++ auth_server_main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = require('./src/server');
-const { listening_port, success } = require("./src/utils");
+const { listening_port, status} = require("./src/utils");
 
 const api = express();
 const Server = new server()
@@ -13,13 +13,13 @@ Server.refreshServerUserArray()
 api.post("/registerUser", (req,res)=>{
 			const userOBJ = {fullname: req.body.name, email: req.body.email, password: req.body.password,
 				 username: req.body.username }
-			Server.registerUser(userOBJ, (result)=>{if(result){res.send("succeded")}else{res.send("failed")}})
+			Server.registerUser(userOBJ, (result)=>{if(result){res.send(status.success)}else{res.send(status.error)}})
 });
 
 api.post("/loginUser", (req, res) => {
-			 Server.SignInUserWithManager(req.body.username, (result)=>{if(result.length > 0){res.send(success)}else{
+			 Server.SignInUserWithManager(req.body.username, (result)=>{if(result.length > 0){res.send(status.succeded)}else{
 				 const userOBJ = {username: req.body.username, password: req.body.password}
-				 Server.loginUser(userOBJ, (response)=>{res.send(success); if(response.valid === true){Server.SignInUser(response);}})
+				 Server.loginUser(userOBJ, (response)=>{res.send(status.succeded); if(response.valid === true){Server.SignInUser(response);}})
 
 			 }})
 });

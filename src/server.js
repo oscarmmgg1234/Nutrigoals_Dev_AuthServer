a7b8e1b95617866366ff5d48edfc0b51a4ff7b64
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-12-27 22:14:31 -0800

    api update

--- src/server.js
+++ src/server.js
@@ -4,13 +4,5 @@ module.exports = class server extends DB{
 constructor(){    
     super()
 }
-signInUserWith(JSONObject){
-    //object = {userID: 2121, signedInTime: moment().add(60, 'minutes')}
-    this.signedInUsers.push(JSONObject)
-   
-}
-checkSignIn(id, callback){
-    this.signedInUsers.map(obj=>{if(obj.username === id){return callback(obj)}})
-    return callback({valid: null})
-}
+//signInManger coming soon
 }
